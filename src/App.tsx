8077860cd9255b9e0e31f3a39567329eb43cdded
sraawan-sai <sraawan.sai@gmail.com>
2023-12-21 05:24:47 +0530

    main folders
     On branch main
     Initial commit
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	new file:   .gitignore
    	new file:   README.md
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   postcss.config.js
    	new file:   public/vite.svg
    	new file:   src/App.css
    	new file:   src/App.tsx
    	new file:   src/assets/react.svg
    	new file:   src/index.css
    	new file:   src/main.tsx
    	new file:   src/vite-env.d.ts
    	new file:   tailwind.config.js
    	new file:   tsconfig.json
    	new file:   tsconfig.node.json
    	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-blue-600'>
+        <p className='text-white font-bold underline'>hello world</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
